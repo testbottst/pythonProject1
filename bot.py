87e094997ee13beaa76df1664f0b05eacd9002c7
Sofiya Moshikocohen Moshiko <moshikocohen@mwsyqs-MacBook.local>
2022-10-22 17:28:58 +0300

    Procfile

--- bot.py
+++ bot.py
@@ -1,69 +1,73 @@
-import constants as keys
-from telegram.ext import *
-import responses as r
 import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
-PORT = int(os.environ.get('PORT', 5000))
+
+PORT = int(os.environ.get('PORT', '8443'))
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
 logger = logging.getLogger(__name__)
-TOKEN = 'keys'
-
-print("הבוט פעיל")
-
-def startCommand(update, context):
-    update.message.reply_text('יש לשלוח את ההודעה הבאה : תפריט או הזמנה כדי להמשיך')
-
-def helpCommand(update, context):
-    update.message.reply_text('@WeDoWeDoo')
-
-def handleMessage(update, context):
-
-    text = str(update.message.text).lower()
+TOKEN = '5528812526:AAEJ715Dc_9Q9ztvZ8tJUX-2xFhhB7IuiJs'
 
 
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    update.message.reply_text('Hi!')
 
-    # Bot response
-    response = r.sampleResponse(text)
 
-    update.message.reply_text(response)
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+
 
 def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
-    # print errors
-    print(f"Update {update} cause error {context.error}")
 
-
-# Run the programme
 def main():
-    updater = Updater(keys.API_Token, use_context=True)
+    """Start the bot."""
+    # Create the Updater and pass it your bot's token.
+    # Make sure to set use_context=True to use the new context based callbacks
+    # Post version 12 this will no longer be necessary
+    updater = Updater(TOKEN, use_context=True)
+
+    # Get the dispatcher to register handlers
     dp = updater.dispatcher
 
-    # Commands
-    dp.add_handler(CommandHandler("start", startCommand))
-    dp.add_handler(CommandHandler("help", helpCommand))
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
 
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, echo))
 
-
-
-    # Messages
-    dp.add_handler(MessageHandler(Filters.text, handleMessage))
-
+    # log all errors
     dp.add_error_handler(error)
 
-
-    # Run the bot
-    updater.idle()
     # Start the Bot
-    updater.start_webhook(listen="0.0.0.0",
-                          port=int(PORT),
-                          url_path=TOKEN)
-    updater.bot.setWebhook('https://intense-escarpment-48006.herokuapp.com/' + TOKEN)
+    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0", port=int(PORT), url_path='TOKEN',
+                          webhook_url='https://intense-escarpment-48006.herokuapp.com/' + TOKEN)
+    # listen="0.0.0.0",
+    # port=int(PORT),
+    # url_path=TOKEN,
+    # webhook_url='https://intense-escarpment-48006.herokuapp.com/' + TOKEN
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
-main()
-
-
+if __name__ == '__main__':
+    main()
