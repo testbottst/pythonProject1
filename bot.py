f09002721834e09a0830ebbe9e36cadf4571a86b
moshikocohen <wedoproseller@gmail.com>
2022-10-23 19:54:25 +0300

    Adjust buildpacks on Heroku Procfile

--- bot.py
+++ bot.py
@@ -60,7 +60,7 @@ def main():
 
     # Start the Bot
     updater.start_webhook(
-        listen="0.0.0.0",
+        listen="127.0.0.1",
         port=int(PORT),
         url_path=TOKEN,
         webhook_url='https://wedobott.herokuapp.com/'+TOKEN)
