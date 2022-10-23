da5791f324c0f777faeefefc58fd4c125f60e7de
moshikocohen <wedoproseller@gmail.com>
2022-10-23 18:16:47 +0300

    Adjust buildpacks on Heroku Procfile

--- bot.py
+++ bot.py
@@ -63,7 +63,7 @@ def main():
         listen="0.0.0.0",
         port=int(PORT),
         url_path=TOKEN,
-        webhook_url=+TOKEN)
+        webhook_url='https://wedobott.herokuapp.com/'+TOKEN)
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
